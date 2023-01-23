f8a3db0d3b8e37df0fa3cbfd1640d09f3a3f3c19
Felix-fale <falefelix23@gmail.com>
2023-01-23 23:29:05 +0000

    feat: Creating the input and button

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
 
 function App() {
-  const [prompt, setPrompt] = useState('')
+  const [prompt, setPrompt] = useState("");
+
   const configuration = new Configuration({
     apiKey: process.env.REACT_APP_OPEINAI_API_KEY,
   });
-  // console.log(configuration);
-  console.log(process.env.REACT_APP_OPEINAI_API_KEY);
 
   const openai = new OpenAIApi(configuration);
 
   const generateImage = async () => {
     const response = await openai.createImage({
-      prompt: "a white siamese cat",
+      prompt: prompt,
       n: 1,
       size: "1024x1024",
     });
@@ -23,7 +22,13 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="app-main">
+      <h3>Generate an image using Open AI API</h3>
+      <input
+        className="app-input"
+        placeholder="Type something to Generate an Image..."
+        onChange={(e) => setPrompt(e.target.value)}
+      />
       <button onClick={generateImage}>Generate an Image</button>
     </div>
   );
