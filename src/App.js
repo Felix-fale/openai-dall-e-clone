93a38d3437eade0cfbc98cb61411c52f9ab4fd82
Felix-fale <falefelix23@gmail.com>
2023-01-23 22:31:41 +0000

    feat: Importing configuration and api keys

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
 
 function App() {
+  const [prompt, setPrompt] = useState('')
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPEINAI_API_KEY,
+  });
+  // console.log(configuration);
+  console.log(process.env.REACT_APP_OPEINAI_API_KEY);
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const response = await openai.createImage({
+      prompt: "a white siamese cat",
+      n: 1,
+      size: "1024x1024",
+    });
+
+    console.log(response.data.data[0].url);
+  };
+
   return (
     <div className="App">
-      <div>aaa {process.env.REACT_APP_API_KEY} </div>
+      <button onClick={generateImage}>Generate an Image</button>
     </div>
   );
 }
